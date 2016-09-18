189b69a13f74f9bff4b4dde092502a99f17af1b9
Thoren Huppke <thuppke@techfak.uni-bielefeld.de>
2016-09-18 18:15:01 +0200

    Class hierarchy and dynamic layers approach

---
+++ Assets/Scripts/RLObject.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RLObject : MonoBehaviour {
+	public List<RLObject> objectsBehind = new List<RLObject> ();
+	public List<RLObject> objectsBefore = new List<RLObject> ();
+	private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer> ();
+//		initializeObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+//	protected abstract void initializeObject ();
+
+	public void addBehind(RLObject other){
+		if (!objectsBehind.Contains (other)) {
+			objectsBehind.Add (other);
+			if (other.getLayerNumber () >= this.getLayerNumber ()) {
+				int oldLayer = this.getLayerNumber ();
+				setLayerNumber (other.getLayerNumber () + 1);
+				updateLayersBefore (oldLayer);
+			}
+			other.addBefore (this);
+		}
+	}
+
+	public void removeBehind(RLObject other){
+		if (objectsBehind.Contains (other)) {
+			objectsBehind.Remove (other);
+			if (other.getLayerNumber () == this.getLayerNumber () - 1) {
+				int oldLayer = this.getLayerNumber ();
+				setLayerNumber (calculateLayer ());
+				updateLayersBefore (oldLayer);
+			}
+			other.removeBefore (this);
+		}
+	}
+
+	private void updateLayersBefore(int oldLayer){
+		foreach (RLObject obj in objectsBefore) {
+			updateLayerNumber (oldLayer);
+		}
+	}
+
+	internal void updateLayerNumber(int oldLayer){
+		if (oldLayer == this.getLayerNumber () - 1) {
+			setLayerNumber (calculateLayer ());
+		}
+	}
+
+	private int calculateLayer() {
+		if (objectsBehind.Count == 0) {
+			return 0;
+		}
+		int max = 0;
+		foreach (RLObject obj in objectsBehind) {
+			if (obj.getLayerNumber () > max) {
+				max = obj.getLayerNumber ();
+			}
+		}
+		return max + 1;
+	}
+
+	private void setLayerNumber(int layer){
+		renderer.sortingOrder = layer;
+	}
+
+	internal int getLayerNumber(){
+		return renderer.sortingOrder;
+	}
+
+	internal void addBefore(RLObject before){
+		if (!objectsBefore.Contains (before)) {
+			objectsBefore.Add (before);
+		}
+	}
+
+	internal void removeBefore(RLObject before) {
+		if (objectsBefore.Contains (before)) {
+			objectsBefore.Remove (before);
+		}
+	}
+}
--- Assets/Scripts/Utility/LayerChangeOnTrigger.cs
+++ Assets/Scripts/Utility/LayerChangeOnTrigger.cs
@@ -2,16 +2,32 @@
 using System.Collections;
 
 public class LayerChangeOnTrigger : MonoBehaviour {
+	private RLObject obj;
+
+	void Start(){
+		obj = GetComponentInParent<RLObject> ();
+	}
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Player") {
-			other.gameObject.SendMessage ("enterTriggerState");
+		if(!other.isTrigger){
+			RLObject otherObject = other.gameObject.GetComponentInParent<RLObject> ();
+			if(otherObject != null)
+				obj.addBehind (otherObject);
+	//		if (other.gameObject.tag == "Player") {
+	//			other.gameObject.SendMessage ("enterTriggerState");
+	//		}
 		}
 	}
 	void OnTriggerExit2D(Collider2D other)
 	{
-		if (other.gameObject.tag == "Player") {
-			other.gameObject.SendMessage("leftTriggerState");
+		if(!other.isTrigger){
+			RLObject otherObject = other.gameObject.GetComponentInParent<RLObject> ();
+			if(otherObject != null)
+				obj.removeBehind (otherObject);
+	//		if (other.gameObject.tag == "Player") {
+	//			other.gameObject.SendMessage("leftTriggerState");
+	//		}
 		}
 	}
 }
